db121c8abe17a7b0768c204287193dd4c0a3a065
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-06-16 11:07:26 +0200

    Added photo on the homepage
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/(www)/page.tsx
+++ src/app/(www)/page.tsx
@@ -1,5 +1,9 @@
+import Image from "next/image";
+
 export default function Index() {
     return <main className={"w-full min-h-[100svh]"}>
-        publieke site
+        <figure className={"container mx-auto pt-5 flex flex-row justify-center"}>
+            <Image className={"w-1/2"} src={"/images/tenboomgaerde.jpg"} alt={""} width={395} height={255}/>
+        </figure>
     </main>;
 }
