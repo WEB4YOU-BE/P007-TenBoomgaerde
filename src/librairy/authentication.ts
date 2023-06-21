ae8f67e9426af0fd6198d7d398a65eb2888980f1
Jens Penneman <jenspenneman26@gmail.com>
2023-06-21 22:21:32 +0200

    [BUGFIX] Pushing until Vercel fucking works 2
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/librairy/authentication.ts
+++ src/librairy/authentication.ts
@@ -14,7 +14,7 @@ export const authenticationOptions: NextAuthOptions = {
                 password: {label: "Wachtwoord", type: "password"},
             },
             async authorize(credentials, req) {
-                const result = await fetch((process.env.VERCEL_URL || "http://localhost:3000") + "/api/login", {
+                const result = await fetch((process.env.NODE_ENV === "production" ? "https://p007-ten-boomgaerde-git-accounts-system-web4you.vercel.app/" : "http://localhost:3000") + "/api/login", {
                     method: "POST",
                     headers: {"Content-type": "application/json"},
                     body: JSON.stringify({
