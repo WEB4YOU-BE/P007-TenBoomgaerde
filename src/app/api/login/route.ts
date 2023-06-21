514db369ba36262f3c2c4cd86526e192ae37f7ba
Jens Penneman <jenspenneman26@gmail.com>
2023-06-21 20:52:57 +0200

    [BUGFIX] POST end-point in caps.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,20 +1,19 @@
 import prisma from "@/librairy/prisma";
-import * as bcrypt from "bcrypt";
+import {compare} from "bcrypt";
 
 interface RequestBody {
     username: string;
     password: string;
 }
 
-export async function Post(request: Request) {
+export async function POST(request: Request) {
     const body: RequestBody = await request.json();
 
     const user = await prisma.user.findFirst({where: {email: body.username}});
 
     if (!user) return new Response(JSON.stringify(null));
-    if (!await bcrypt.compare(body.password, user.password)) return new Response(JSON.stringify(null));
+    if (!await compare(body.password, user.password)) return new Response(JSON.stringify(null));
 
     const {password, ...userWithoutPassword} = user;
     return new Response(JSON.stringify(userWithoutPassword));
-
 }
