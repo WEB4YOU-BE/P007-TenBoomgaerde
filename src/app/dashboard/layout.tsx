c5fb93e419ee967139e17aa82efe70dfdf41f434
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-06-22 11:23:39 +0200

    Working navigation on desktop and mobile (bugfix)
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -16,7 +16,7 @@ export default function PublicNavigationLayoutDashboard({children}: { children: 
                             d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"></path>
                     </svg>
                 </CollapsibleTrigger>
-                <Link href={"/dashboard"} className={"flex flex-row gap-2 p-2 block"}>
+                <Link href={"/dashboard"} className={"flex flex-row gap-2 p-2"}>
                     <Image className={"w-[40px] h-[40px] rounded-full"}
                            src={"/images/logo-bewegingnet - square.png"}
                            alt={"Logo"} width={40} height={40}/>
