a0e8050147079cae3cb5a64284838867207791b1
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-06-22 15:49:36 +0200

    Code for the footer added in the components
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/(www)/layout.tsx
+++ src/app/(www)/layout.tsx
@@ -1,21 +1,10 @@
 import Navigation from "@/components/ui/www/Navigation";
-import Link from "next/link";
+import Footer from "@/components/ui/www/Footer";
 
 export default function PublicNavigationLayout({children}: { children: React.ReactNode }) {
     return <>
         <Navigation/>
         {children}
-        <footer className={"flex flex-col md:flex-row bg-gray-200 rounded-lg shadow m-4 container"}>
-            <span className={"p-4 text-sm text-gray-500 text-center"}>&copy; Made by{" "}
-                <Link href={"https://web-4-you.be/"}>WEB4YOU</Link></span>
-            <div
-                className={"p-4 flex flex-row gap-4 text-sm font-medium text-gray-500 justify-between md:ml-auto"}>
-                <Link className={"hover:underline"}
-                      href={"https://www.google.com/maps/place/Zaal+Ten+Boomgaerde/@51.0280964,3.1406539,15z/data=!4m6!3m5!1s0x47c34bdf2550654d:0xa6fb68fa7774a37e!8m2!3d51.0280964!4d3.1406539!16s%2Fg%2F1t_hy456?entry=ttu"}
-                      target={"_blank"}>Boomgaerdstraat 4a, 8810 Lichtervelde</Link>
-                <Link className={"hover:underline"}
-                      href={"mailto:info@vzwtenboomgaerdelichtervelde.be"}>Contact</Link>
-            </div>
-        </footer>
+        <Footer/>
     </>;
 }
