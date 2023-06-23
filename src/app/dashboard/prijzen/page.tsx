19e15c776a9f6fd523f0214c413e9fc61cd9e9b4
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-06-23 14:21:27 +0200

    Added tablehead
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/dashboard/prijzen/page.tsx
+++ src/app/dashboard/prijzen/page.tsx
@@ -23,7 +23,7 @@ export default function Index() {
                                 <path
                                     d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"></path>
                             </svg>
-                            Refresh
+                            Bijwerken
                         </button>
                         <button
                             className={"inline-flex items-center justify-center w-1/2 px-3 py-2 text-sm font-medium text-center rounded-lg bg-green-200 hover:bg-green-300 focus:ring-4 focus:ring-green-300 sm:w-auto"}>
@@ -35,6 +35,36 @@ export default function Index() {
                             </svg>
                             Toevoegen
                         </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={"flex flex-col"}>
+            <div className={"overflow-x-auto"}>
+                <div className={"inline-block min-w-full align-middle"}>
+                    <div className={"overflow-hidden shadow"}>
+                        <table className={"min-w-full divide-y divide-gray-200 table-fixed"}>
+                            <thead className={"bg-gray-100"}>
+                            <tr>
+                                {
+                                    [
+                                        'Naam',
+                                        'Prijs',
+                                        'Categorie',
+                                        'Acties'
+                                    ].map((th) => (
+                                        <th scope={"col"}
+                                            className={"p-4 text-xs font-medium text-left text-gray-500 uppercase"}>
+                                            {th}
+                                        </th>
+                                    ))
+                                }
+                            </tr>
+                            </thead>
+                            <tbody className={"divide-y divide-gray-200"}>
+
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
