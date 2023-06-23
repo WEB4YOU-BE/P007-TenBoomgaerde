ede79fc802948ca2343e5376361ac52c916db695
Tiebe Deweerdt <74412629+TiebeDW@users.noreply.github.com>
2023-06-23 13:01:09 +0200

    WWW Footer added #7
    * footer created with css in layout.tsx
    
    * Code for the footer added in the components
    
    * Footer is centered on big screens

--- src/app/(www)/layout.tsx
+++ src/app/(www)/layout.tsx
@@ -1,8 +1,10 @@
 import Navigation from "@/components/ui/www/Navigation";
+import Footer from "@/components/ui/www/Footer";
 
 export default function PublicNavigationLayout({children}: { children: React.ReactNode }) {
     return <>
         <Navigation/>
         {children}
+        <Footer/>
     </>;
 }
