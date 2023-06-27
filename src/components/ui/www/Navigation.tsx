7b6b804d67e4f401f3939eeff9c872c362938dd3
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-06-27 15:37:55 +0200

    Added the new logo on the navigation
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/components/ui/www/Navigation.tsx
+++ src/components/ui/www/Navigation.tsx
@@ -12,7 +12,8 @@ const Navigation = async () => {
     return <div className={"sm:sticky sm:top-0 sm:z-50 container max-w-screen-md mx-auto p-2"}>
         <div className={"bg-gray-200 rounded-lg flex flex-col gap-2 p-2"}>
             <div className={"flex flex-row gap-2"}>
-                <Image className={"aspect-square w-[40px] h-[40px] rounded-lg"} src={"/images/logo-bewegingnet - square.png"} alt={"Logo"} width={77} height={77}/>
+                <Image className={"aspect-square w-[40px] h-[40px] rounded-lg"} src={"/images/Logo Ten Boomgaerde.PNG"}
+                       alt={"Logo"} width={77} height={77}/>
                 <nav className={"flex flex-row flex-grow overflow-x-auto invisible sm:visible gap-2"}>
                     <Link className={buttonVariants({variant: "ghost"})} href={"/"}>Startpagina</Link>
                     <Link className={buttonVariants({variant: "ghost"})} href={"/reserveren"}>Reserveer</Link>
