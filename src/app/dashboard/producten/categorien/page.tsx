0e04264883eb9917550fcc517c698417daa19acc
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-07-27 12:05:47 +0200

    Typo
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/dashboard/producten/categorien/page.tsx
+++ src/app/dashboard/producten/categorien/page.tsx
@@ -6,7 +6,7 @@ export default function Index() {
         <div className={"p-4 block sm:flex items-center justify-between"}>
             <div className={"w-full mb-1"}>
                 <div className={"mb-4"}>
-                    <h1 className={"text-xl font-semibold text-gray-900 sm:text-2xl"}>Categorien</h1>
+                    <h1 className={"text-xl font-semibold text-gray-900 sm:text-2xl"}>Categorieën</h1>
                 </div>
                 <div className={"items-center justify-between block sm:flex"}>
                     <div className={"flex items-center mb-4 sm:mb-0"}>
