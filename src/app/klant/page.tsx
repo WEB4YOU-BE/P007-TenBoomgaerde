a4d74ce064cfbb58010fb2cc9472df133a7d9dfb
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-08-11 11:05:46 +0200

    Reservation card without to much layout
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/klant/page.tsx
+++ src/app/klant/page.tsx
@@ -1,5 +1,14 @@
+import Reservation from "@/components/ui/klant/Reservation";
+
 export default function Index() {
     return <main className={"w-full min-h-[100svh]"}>
-        <p>Dat is het klant dashboard</p>
+        <div className={"p-4 block sm:flex items-center justify-between"}>
+            <div className={"w-full mb-1"}>
+                <div className={"mb-4"}>
+                    <h1 className={"text-xl font-semibold text-gray-900 sm:text-2xl"}>Mijn boekingen</h1>
+                </div>
+                <Reservation/>
+            </div>
+        </div>
     </main>;
 }
---
+++ src/components/ui/klant/Reservation.tsx
@@ -0,0 +1,36 @@
+import {Badge} from "@/components/ui/Badge";
+
+const Reservation = async () => {
+    return <div className={"p-4 border border-gray-200 rounded-lg shadow-sm"}>
+        <div className={"grid gap-4 grid-cols-3 grid-rows-4"}>
+            <div className={"font-bold text-xl row-start-1 col-span-2"}>
+                Reservatienummer: <span>21457</span>
+            </div>
+            <span className={"flex justify-end text-sm"}>
+                <Badge variant={"success"}>Goedgekeurd</Badge>
+            </span>
+            <div className={"sm:row-start-2 col-span-3 sm:col-span-2"}>
+                <span className={"font-bold"}>Reserveerder: </span>
+                <span>Tiebe Deweerdt</span>
+            </div>
+            <div className={"sm:row-start-3 col-span-3 sm:col-span-2"}>
+                <span className={"font-bold"}>Datum: </span>
+                <span>11-08-2023 van 13:00 tot 21:00</span>
+            </div>
+            <div className={"sm:row-start-4 col-span-3 sm:col-span-2"}>
+                <span className={"font-bold"}>Zaal: </span>
+                <span>Grote zaal</span>
+            </div>
+            <div className={"sm:row-start-3 col-span-3 sm:col-span-1"}>
+                <span className={"font-bold"}>GSM'nr voor poort: </span>
+                <span>+32 471 71 09 91</span>
+            </div>
+            <div className={"sm:row-start-4 col-span-3 sm:col-span-1"}>
+                <span className={"font-bold"}>Code sleutel: </span>
+                <span>Nog niet bekend</span>
+            </div>
+        </div>
+    </div>
+}
+
+export default Reservation;
