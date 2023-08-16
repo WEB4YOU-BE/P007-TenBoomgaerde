c0f840f7db3e611b65f451634bfbf84de3d3ce5e
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-08-16 11:21:27 +0200

    Buttons to save zaal settings and button to turn back
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/dashboard/zalen/[zaalId]/page.tsx
+++ src/app/dashboard/zalen/[zaalId]/page.tsx
@@ -16,7 +16,7 @@ export default function Index() {
                 </div>
                 <div className={"grid sm:grid-cols-2 grid-cols-1 gap-4"}>
                     <form
-                        className={"grid grid-cols-2 w-full gap-4 p-4 my-4 border border-gray-200 rounded-lg shadow-sm max-h-44"}>
+                        className={"grid grid-cols-2 w-full gap-4 p-4 my-4 border border-gray-200 rounded-lg shadow-sm max-h-60"}>
                         <label
                             className={" my-auto font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 mr-4 "}
                         >Naam</label>
@@ -36,6 +36,12 @@ export default function Index() {
                                   className={"flex justify-center p-1 w-full border border-gray-200 rounded-full shadow-sm bg-green-200 hover:bg-green-300"}>NM: &euro; 70</Link>
                             <Link href={`/dashboard/zalen/${zaal[0].id}/3`} id={"3"}
                                   className={"flex justify-center p-1 w-full border border-gray-200 rounded-full shadow-sm bg-green-200 hover:bg-green-300"}>AV: &euro; 70</Link>
+                        </div>
+                        <div className={"col-span-2 flex flex-row justify-between"}>
+                            <Link href={"/dashboard/zalen/"}
+                                  className={"items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-green-200 hover:bg-green-300 focus:ring-4 focus:ring-green-300"}>Terug</Link>
+                            <Link href={"/dashboard/zalen/"}
+                                  className={"items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-green-200 hover:bg-green-300 focus:ring-4 focus:ring-green-300"}>Opslaan</Link>
                         </div>
                     </form>
                     <div className={"bg-gray-200 flex-grow h-72 rounded p-2"}>React Kalender</div>
