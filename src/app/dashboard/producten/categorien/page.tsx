2a5b49009be4dd72f4ea77c2c5ff7a3e5a43860b
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-08-21 20:12:02 +0200

    Page for change or add a new categorie
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- src/app/dashboard/producten/categorien/page.tsx
+++ src/app/dashboard/producten/categorien/page.tsx
@@ -1,7 +1,14 @@
 import {Dialog, DialogTrigger} from "@/components/ui/Dialog";
 import CategorieDialog from "@/components/ui/dashboard/CategorieDialog";
+import Link from "next/link";
 
 export default function Index() {
+
+    const categories = [
+        {id: 1, name: "Drank"},
+        {id: 2, name: "Materiaal"},
+    ]
+
     return <main className={"w-full min-h-[100svh]"}>
         <div className={"p-4 block sm:flex items-center justify-between"}>
             <div className={"w-full mb-1"}>
@@ -66,7 +73,54 @@ export default function Index() {
                             </tr>
                             </thead>
                             <tbody className={"divide-y divide-gray-200"}>
-
+                            {categories.map((categories) => (
+                                <tr className={"hover:bg-gray-100"} key={categories.id}>
+                                    <td className={"p-4 text-base font-medium text-gray-900 whitespace-nowrap"}>
+                                        <data value={"naam"}>{categories.name}</data>
+                                    </td>
+                                    <td className={"p-4 space-x-2 whitespace-nowrap"}>
+                                        <Link
+                                            className={"inline-flex items-center px-3 py-2 text-sm font-medium text-center rounded-lg bg-green-200 hover:bg-green-300 focus:ring-4 focus:ring-green-300"}
+                                            href={`/dashboard/producten/categorien/${categories.id}`}
+                                        >
+                                            <svg
+                                                className={"w-4 h-4"}
+                                                fill={"currentColor"}
+                                                viewBox={"0 0 20 20"}
+                                                xmlns={"http://www.w3.org/2000/svg"}
+                                            >
+                                                <svg>
+                                                    <path
+                                                        d={"M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"}/>
+                                                    <path
+                                                        fillRule={"evenodd"}
+                                                        d={"M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z"}
+                                                        clipRule={"evenodd"}
+                                                    />
+                                                </svg>
+                                            </svg>
+                                            <span className={"sm:ml-2 hidden sm:block"}>Bewerk</span>
+                                        </Link>
+                                        <button
+                                            className={"inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300"}
+                                        >
+                                            <svg
+                                                className={"w-4 h-4"}
+                                                fill={"currentColor"}
+                                                viewBox={"0 0 20 20"}
+                                                xmlns={"http://www.w3.org/2000/svg"}
+                                            >
+                                                <path
+                                                    fillRule={"evenodd"}
+                                                    d={"M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"}
+                                                    clipRule={"evenodd"}
+                                                />
+                                            </svg>
+                                            <span className={"sm:ml-2 hidden sm:block"}>Verwijder</span>
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
                             </tbody>
                         </table>
                     </div>
