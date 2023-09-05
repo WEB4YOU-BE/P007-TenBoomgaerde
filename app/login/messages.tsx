f1e699e654a7e053c22a763dcbfc66e62decd2dc
Jens Penneman <jenspenneman26@gmail.com>
2023-09-05 14:19:37 +0200

    Undocumented changes
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -6,18 +6,8 @@ export default function Messages() {
     const searchParams = useSearchParams()
     const error = searchParams.get('error')
     const message = searchParams.get('message')
-    return (
-        <>
-            {error && (
-                <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
-                    {error}
-                </p>
-            )}
-            {message && (
-                <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
-                    {message}
-                </p>
-            )}
-        </>
-    )
+    return <>
+        {error && <p className="mt-2 border border-red-400 bg-red-100 rounded-lg p-2">{error}</p>}
+        {message && <p className="mt-2 border border-blue-400 bg-blue-100 rounded-lg p-2">{message}</p>}
+    </>
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,25 @@
-import Link from 'next/link'
 import Messages from './messages'
 
 export default function Login() {
+    return <form action={"/auth/sign-in"} method={"POST"}
+                 className={"rounded-lg border bg-white text-black grid gap-4 p-4"}>
+        <Messages/>
+        <div className={"flex flex-col gap-1"}>
+            <label htmlFor={"email"}
+                   className={"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"}>Email</label>
+            <input required id={"email"} name={"email"} placeholder={"jij@voorbeeld.com"}
+                   className={"flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}/>
+        </div>
+        <div className={"flex flex-col gap-1"}>
+            <label htmlFor={"password"}
+                   className={"text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"}>Wachtwoord</label>
+            <input required id={"password"} name={"password"} type={"password"} autoComplete={"current-password"} placeholder="••••••••"
+                   className={"flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}/>
+        </div>
+        <button>Login</button>
+    </form>
+
+    /*
     return (
         <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
             <Link
@@ -63,4 +81,5 @@ export default function Login() {
             </form>
         </div>
     )
+     */
 }
