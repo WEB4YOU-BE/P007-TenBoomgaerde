9fc50632373920978d65104aec5904d2b072f2c6
Jens Penneman <jenspenneman26@gmail.com>
2023-09-05 11:15:52 +0200

    Reset NextJS app
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- src/app/api/auth/[...nextauth]/route.ts
+++
@@ -1,6 +0,0 @@
-import NextAuth from "next-auth";
-import {authenticationOptions} from "@/librairy/authentication";
-
-const handler = NextAuth(authenticationOptions);
-
-export {handler as GET, handler as POST};
