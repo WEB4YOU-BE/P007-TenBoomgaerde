b0f7eaf2fb22642ecc48a1c03ab006278226aec7
Jens Penneman <jenspenneman26@gmail.com>
2023-09-07 17:48:55 +0200

    Fixed edge-runtime error in Vercel
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,8 @@
 import Messages from './messages'
 import {buttonVariants} from "@/components/ui/button";
 
+export const dynamic = 'force-dynamic'
+
 export default function Login() {
     return <form action={"/auth/sign-in"} method={"POST"}
                  className={"w-full sm:w-1/2 md:w-1/3 lg:w-1/4 rounded-lg border bg-white text-black grid gap-4 p-4"}>
