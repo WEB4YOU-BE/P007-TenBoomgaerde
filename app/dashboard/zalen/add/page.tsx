f806f2ba6687377edcf733b1332df8a2733be241
Jens Penneman <jenspenneman26@gmail.com>
2023-09-17 16:08:13 +0200

    Added spacing to the page.tsx for zalen toevoegen.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/dashboard/zalen/add/page.tsx
+++ app/dashboard/zalen/add/page.tsx
@@ -1,7 +1,8 @@
 import AddRoomForm from "@/components/business/rooms/add-room-form";
 
 export default async function page() {
-    return <main>
+    return <main className={"mx-auto md:max-w-screen-sm p-2 flex flex-col gap-2"}>
+        <h1 className={"scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl md:flex-grow flex-shrink-0"}>Voeg een zaal toe</h1>
         <AddRoomForm/>
     </main>
 }
