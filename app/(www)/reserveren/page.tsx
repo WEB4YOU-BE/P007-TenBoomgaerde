60762a349f9b1368f06f7950a0c03b2eef21ba69
Jens Penneman <jenspenneman26@gmail.com>
2023-09-18 16:14:05 +0200

    Changed everything to the same container width.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/(www)/reserveren/page.tsx
+++ app/(www)/reserveren/page.tsx
@@ -2,7 +2,7 @@ import Calender from "@/components/ui/www/Calender";
 
 export default async function Index() {
     return <main className={"w-full min-h-[calc(100svh-72px)]"}>
-        <div className={"container mx-auto"}>
+        <div className={" container max-w-screen-xl mx-auto p-2 px-4"}>
             <h1>Reserveren</h1>
             <Calender/>
         </div>
