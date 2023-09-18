d819b8edd3c050cc772f2f87041578a46d2a18fe
Jens Penneman <jenspenneman26@gmail.com>
2023-09-18 18:20:09 +0200

    Small "Tiebe" update to category-add- and room-add-form.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- components/business/categories/add-category-form.tsx
+++ components/business/categories/add-category-form.tsx
@@ -14,7 +14,7 @@ export default async function AddCategoryForm() {
         const supabase = createServerComponentClient({cookies})
         await supabase.from("categories").insert({name: name})
 
-        redirect("/dashboard/producten/categorien", RedirectType.push)
+        redirect("/dashboard/producten/categorieen", RedirectType.push)
     }
 
     return <form action={onCreateCategory} className={"flex flex-col gap-2"}>
@@ -24,6 +24,6 @@ export default async function AddCategoryForm() {
             <input autoFocus required id={"name"} name={"name"} placeholder={"Verhuur"}
                    className={"flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}/>
         </div>
-        <button type={"submit"} className={buttonVariants()}>Maak aan</button>
+        <button type={"submit"} className={buttonVariants({variant: "green"})}>Maak aan</button>
     </form>
 }
