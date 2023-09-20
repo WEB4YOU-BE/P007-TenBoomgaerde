1b03e21f6c72248f1b35e8a74e2b70aab146c6f9
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-09-20 22:03:42 +0200

    Made a confirmation page, everything works with mockdata. Next is working with the db
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- app/(www)/reserveren/[roomId]/[blokId]/overview/page.tsx
+++ app/(www)/reserveren/[roomId]/[blokId]/overview/page.tsx
@@ -1,5 +1,6 @@
 import {buttonVariants} from "@/components/ui/button";
 import {cn} from "@/lib/utils";
+import Link from "next/link";
 
 export default function Index() {
     return <main className={"w-full min-h-[calc(100svh-72px)]"}>
@@ -35,7 +36,7 @@ export default function Index() {
                 <span>BE 1234 456 789</span>
             </div>
             <div className={"flex justify-end my-8"}>
-                <button className={cn(buttonVariants({variant: "green"}), "w-1/3")}>Bevestig</button>
+                <Link href={"/bevestiging"} className={cn(buttonVariants({variant: "green"}), "w-1/3")}>Bevestig</Link>
             </div>
             <div className={"bg-red-100 border-red-400 border-2 rounded p-2 mb-8"}>Na klikken op bevestigen krijgt u een
                 mail met dat uw reservatie is aangevraagd. Pas na dat we uw reservatie hebben goedgekeurd is uw
