b347b31b4836ebaa37ade216d230df20678acc11
Jens Penneman <jenspenneman26@gmail.com>
2023-09-28 18:27:09 +0200

    Added overview button in overview itself + protection for dashboard button.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/klant/layout.tsx
+++ app/klant/layout.tsx
@@ -2,9 +2,11 @@ import * as React from "react"
 
 import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 import NavigationSidebarAuthentication from "@/components/authentication/navigation-sidebar-authentication";
-import {GanttChart} from "lucide-react";
+import {LayoutDashboard, List} from "lucide-react";
 import NavigationSidebarLink from "@/components/navigation/navigation-sidebar-link";
 import LoginRouteProtection from "@/components/authentication/login-route-protection";
+import AdminComponentProtection from "@/components/authentication/admin-component-protection";
+import LoginComponentProtection from "@/components/authentication/login-component-protection";
 
 export const dynamic = 'force-dynamic'
 
@@ -16,7 +18,8 @@ export default function PublicNavigationLayoutDashboard({children}: LayoutProps)
     return <LoginRouteProtection>
         <div className={"flex flex-col md:flex-row"}>
             <NavigationSidebar authNode={<NavigationSidebarAuthentication/>}>
-                <NavigationSidebarLink href={"/dashboard"}><GanttChart/><span>Dashboard</span></NavigationSidebarLink>
+                <LoginComponentProtection><NavigationSidebarLink href={"/klant"}><List/><span>Mijn overzicht</span></NavigationSidebarLink></LoginComponentProtection>
+                <AdminComponentProtection><NavigationSidebarLink href={"/dashboard"}><LayoutDashboard/><span>Dashboard</span></NavigationSidebarLink></AdminComponentProtection>
             </NavigationSidebar>
             <div className={"w-full"}>
                 {children}
