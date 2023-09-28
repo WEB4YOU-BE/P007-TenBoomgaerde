44c1f1cddb0531dec5d90a1bec56d7f72a49c217
Jens Penneman <35633281+JensPenneman@users.noreply.github.com>
2023-09-28 18:35:24 +0200

    Added small QoL improvements to the "klant" UI. (#39)
    * Only clients can access there overview.
    
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>
    
    * Overview button in navigation is added for normal- and admin-users.
    
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>
    
    * Added overview button in overview itself + protection for dashboard button.
    
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>
    
    ---------
    
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/klant/layout.tsx
+++ app/klant/layout.tsx
@@ -2,8 +2,11 @@ import * as React from "react"
 
 import NavigationSidebar from "@/components/navigation/navigation-sidebar";
 import NavigationSidebarAuthentication from "@/components/authentication/navigation-sidebar-authentication";
-import {GanttChart} from "lucide-react";
+import {LayoutDashboard, List} from "lucide-react";
 import NavigationSidebarLink from "@/components/navigation/navigation-sidebar-link";
+import LoginRouteProtection from "@/components/authentication/login-route-protection";
+import AdminComponentProtection from "@/components/authentication/admin-component-protection";
+import LoginComponentProtection from "@/components/authentication/login-component-protection";
 
 export const dynamic = 'force-dynamic'
 
@@ -12,12 +15,15 @@ interface LayoutProps {
 }
 
 export default function PublicNavigationLayoutDashboard({children}: LayoutProps) {
-    return <div className={"flex flex-col md:flex-row"}>
-        <NavigationSidebar authNode={<NavigationSidebarAuthentication/>}>
-            <NavigationSidebarLink href={"/dashboard"}><GanttChart/><span>Dashboard</span></NavigationSidebarLink>
-        </NavigationSidebar>
-        <div className={"w-full"}>
-            {children}
+    return <LoginRouteProtection>
+        <div className={"flex flex-col md:flex-row"}>
+            <NavigationSidebar authNode={<NavigationSidebarAuthentication/>}>
+                <LoginComponentProtection><NavigationSidebarLink href={"/klant"}><List/><span>Mijn overzicht</span></NavigationSidebarLink></LoginComponentProtection>
+                <AdminComponentProtection><NavigationSidebarLink href={"/dashboard"}><LayoutDashboard/><span>Dashboard</span></NavigationSidebarLink></AdminComponentProtection>
+            </NavigationSidebar>
+            <div className={"w-full"}>
+                {children}
+            </div>
         </div>
-    </div>
+    </LoginRouteProtection>
 }
