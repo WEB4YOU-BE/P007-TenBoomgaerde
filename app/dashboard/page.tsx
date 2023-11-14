006094a62346e8d0f6fd66622e068d3de6ec79e4
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-11-14 19:23:35 +0100

    Made a extra card for a line chart later
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -97,6 +97,25 @@ export default async function page() {
                     </HoverCard>
                     <p className={"text-6xl font-bold text-green-600 sm:text-8xl text-center"}>{reservationsInvoice.data.length}</p>
                 </div>
+                <div
+                    className={"p-4 border border-gray-200 rounded-lg shadow-sm col-span-2 xl:col-span-3 2xl:col-span-4 sm:p-6 xl:row-span-2"}>
+                    <div className="flex items-center justify-between mb-4">
+                        <div className="flex-shrink-0">
+                            <h3
+                                className="text-xl font-bold leading-none text-gray-900 sm:text-2xl dark:text-white">Reserveringen</h3>
+                        </div>
+                        {/*<div
+                            className="flex items-center justify-end flex-1 text-base font-medium text-green-500 dark:text-green-400">
+                            12.5%
+                            <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                      d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L6.707 7.707a1 1 0 01-1.414 0z"
+                                      clip-rule="evenodd"></path>
+                            </svg>
+                        </div>*/}
+                    </div>
+                </div>
                 <div className={"p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6  lg:col-start-5"}>
                     <HoverCard>
                         <HoverCardTrigger asChild>
