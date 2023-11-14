f4ee230dce909102e0a89166c5c3c12a0c868c69
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-11-14 22:07:16 +0100

    Add export to excel
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- app/dashboard/analyses/page.tsx
+++ app/dashboard/analyses/page.tsx
@@ -6,6 +6,7 @@ import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
 import {Printer} from "lucide-react";
 import ReservationsTable from "@/components/business/reservations/reservations-table";
+import ExportExcel from "@/components/business/export-excel";
 
 function getWeekDates() {
     const now = new Date()
@@ -13,6 +14,13 @@ function getWeekDates() {
     const end = new Date(start)
     end.setDate(start.getDate() + 6)
     return {start, end}
+}
+
+function getMonthDates() {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), now.getMonth(), 1);
+    const end = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    return {start, end};
 }
 
 function formatDate({date}: {
@@ -26,21 +34,36 @@ function formatDate({date}: {
 
 export default async function page() {
     const supabase = createServerComponentClient({cookies})
-    const query = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd`).gte('start_date', formatDate({date: getWeekDates().start})).lte('end_date', formatDate({date: getWeekDates().end})).order('start_date')
-    const reservations: DbResult<typeof query> = await query
+    const queryWeek = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd`).gte('start_date', formatDate({date: getWeekDates().start})).lte('end_date', formatDate({date: getWeekDates().end})).order('start_date')
+    const reservationsWeek: DbResult<typeof queryWeek> = await queryWeek
+    const queryMonth = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd`).gte('start_date', formatDate({date: getMonthDates().start})).lte('end_date', formatDate({date: getMonthDates().end})).order('start_date')
+    const reservationsMonth: DbResult<typeof queryMonth> = await queryMonth
 
-    if (!reservations.data) return undefined
+    if (!reservationsWeek.data) return undefined
+    if (!reservationsMonth.data) return undefined
 
     return <main className={"flex flex-col gap-2"}>
         <div className={"m-5 border border-gray-300 rounded-2xl p-2"}>
             <div className={"flex flex-col md:flex-row gap-2 p-4"}>
                 <h1 className={"text-4xl font-extrabold tracking-tight lg:text-5xl md:flex-grow"}>Reservaties deze
                     week</h1>
+                <ExportExcel reservations={reservationsWeek.data}/>
                 <Link href={"/dashboard/analyses/weekPDF"}
                       className={cn(buttonVariants({variant: "green"}), "flex flex-row gap-2")}><Printer
                     size={16}/>Afdrukken</Link>
             </div>
-            <ReservationsTable reservations={reservations.data}/>
+            <ReservationsTable reservations={reservationsWeek.data}/>
+        </div>
+        <div className={"m-5 border border-gray-300 rounded-2xl p-2"}>
+            <div className={"flex flex-col md:flex-row gap-2 p-4"}>
+                <h1 className={"text-4xl font-extrabold tracking-tight lg:text-5xl md:flex-grow"}>Reservaties deze
+                    maand</h1>
+                <ExportExcel reservations={reservationsMonth.data}/>
+                {/*<Link href={"/dashboard/analyses/weekPDF"}
+                      className={cn(buttonVariants({variant: "green"}), "flex flex-row gap-2")}><Printer
+                    size={16}/>Afdrukken</Link>*/}
+            </div>
+            <ReservationsTable reservations={reservationsMonth.data}/>
         </div>
     </main>
 }
