30e46ea909dcf9904b99d2e782ac454b71fc23df
Jens Penneman <jenspenneman26@gmail.com>
2023-11-29 20:50:10 +0100

    Added the NotFound SEO
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,6 +3,45 @@ import {cn} from "@/lib/utils";
 import {buttonVariants} from "@/components/ui/button";
 import Link from "next/link";
 
+export const metadata = {
+    title: "Deze pagina bestaat niet",
+    description: 'Deze pagina maakt geen deel uit van de website voor VZW Ten Boomgaerde Lichtervelde.',
+
+    applicationName: "VZW Ten Boomgaerde Lichtervelde",
+    keywords: ["Ten Boomgaerde", "Lichtervelde", "VZW"],
+
+    creator: "WEB4YOU",
+    publisher: "WEB4YOU",
+    authors: [{name: "Jens Penneman", url: "https://jenspenneman.com"}],
+
+    colorScheme: "light dark",
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "#e7e5e4"},
+        {media: "(prefers-color-scheme: dark)", color: "#292524"},
+    ],
+    formatDetection: {
+        url: false,
+        email: false,
+        telephone: false,
+        address: false,
+        date: false,
+    },
+
+    metadataBase: new URL("https://www.vzwtenboomgaerdelichtervelde.be"),
+    referrer: "origin-when-cross-origin",
+    alternates: {
+        canonical: "/",
+        languages: {},
+    },
+
+    appleWebApp: {
+        title: "VZW Ten Boomgaerde Lichtervelde",
+        statusBarStyle: "default",
+    },
+
+    generator: "Next.js",
+};
+
 export default function NotFound() {
     return <main className={"flex flex-col m-28"}>
         <Image src={"/images/Logo Ten Boomgaerde.PNG"} alt={"logo"} width={1000} height={1000}
