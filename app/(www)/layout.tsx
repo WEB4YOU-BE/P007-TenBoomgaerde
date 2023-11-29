8886491696c81af76dbad2ed269fe2af8c0e86b2
Jens Penneman <jenspenneman26@gmail.com>
2023-11-29 20:44:18 +0100

    Added the default SEO to layout.tsx
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/(www)/layout.tsx
+++ app/(www)/layout.tsx
@@ -5,6 +5,48 @@ import NavigationFooter from "@/components/navigation/navigation-footer";
 
 export const dynamic = 'force-dynamic'
 
+export const metadata = {
+    title: {
+        template: '%s | VZW Ten Boomgaerde Lichtervelde',
+        default: 'VZW Ten Boomgaerde Lichtervelde',
+        absolute: 'VZW Ten Boomgaerde Lichtervelde',
+    },
+
+    applicationName: "VZW TBL",
+    keywords: ["Ten Boomgaerde", "Lichtervelde", "VZW"],
+
+    creator: "WEB4YOU",
+    publisher: "WEB4YOU",
+    authors: [{name: "Jens Penneman", url: "https://jenspenneman.com"}],
+
+    colorScheme: "light dark",
+    themeColor: [
+        {media: "(prefers-color-scheme: light)", color: "#e7e5e4"},
+        {media: "(prefers-color-scheme: dark)", color: "#292524"},
+    ],
+    formatDetection: {
+        url: false,
+        email: false,
+        telephone: false,
+        address: false,
+        date: false,
+    },
+
+    metadataBase: new URL("https://www.vzwtenboomgaerdelichtervelde.be"),
+    referrer: "origin-when-cross-origin",
+    alternates: {
+        canonical: "/",
+        languages: {},
+    },
+
+    appleWebApp: {
+        title: "VZW Ten Boomgaerde Lichtervelde",
+        statusBarStyle: "default",
+    },
+
+    generator: "Next.js",
+};
+
 export default async function PublicNavigationLayout({children}: { children: React.ReactNode }) {
     return <>
         <NavigationHeader authNode={<NavigationHeaderAuthentication/>}>
