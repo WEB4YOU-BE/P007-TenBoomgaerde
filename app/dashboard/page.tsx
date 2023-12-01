4c03e23fc3ed63af44bfd77c1fe17885aa105932
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2023-12-01 21:12:18 +0100

    Made the dashboard responsive and ready for live
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -14,6 +14,13 @@ function getWeekDates() {
     return {start, end}
 }
 
+function getMonthDates() {
+    const now = new Date();
+    const start = new Date(now.getFullYear(), now.getMonth(), 1);
+    const end = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+    return {start, end};
+}
+
 function formatDate({date}: {
     date: Date
 }) {
@@ -27,84 +34,74 @@ export default async function page() {
     const supabase = createServerComponentClient({cookies})
     const queryHold = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd, organizations(name)`).eq('status', 'in afwachting').order('start_date')
     const queryWeak = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd, organizations(name)`).gte('start_date', formatDate({date: getWeekDates().start})).lte('end_date', formatDate({date: getWeekDates().end})).order('start_date')
+    const queryMonth = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd, organizations(name)`).gte('start_date', formatDate({date: getMonthDates().start})).lte('end_date', formatDate({date: getMonthDates().end})).order('start_date')
     const queryInvoice = supabase.from("reservations").select(`id, reservation_year, reservation_number, users(id, firstname, lastname), rooms(name), start_hour:bloks!start_hour(start_hour), end_hour:bloks!end_hour(end_hour), start_date, end_date, products(name), access_code, status, gefactureerd, organizations(name)`).eq('gefactureerd', 'FALSE').order('start_date')
 
     const reservationsHold: DbResult<typeof queryHold> = await queryHold
     const reservationsWeak: DbResult<typeof queryWeak> = await queryWeak
+    const reservationsMonth: DbResult<typeof queryMonth> = await queryMonth
     const reservationsInvoice: DbResult<typeof queryInvoice> = await queryInvoice
 
     if (!reservationsHold.data) return undefined
     if (!reservationsWeak.data) return undefined
+    if (!reservationsMonth.data) return undefined
     if (!reservationsInvoice.data) return undefined
 
     return <main className={"flex flex-col gap-2"}>
-        <div className={"px-4 pt-6"}>
-            <div className={"grid gap-4 grid-cols-2 xl:grid-cols-4 2xl:grid-cols-5 grid-rows-2"}>
-                <div
-                    className={"p-4 border border-gray-200 rounded-lg shadow-sm col-span-2 xl:col-span-3 2xl:col-span-4 sm:p-6 xl:row-span-2"}>
-                    <Tabs defaultValue={"Deze week"}>
-                        <div className={"mb-4 border-b border-gray-200"}>
-                            <TabsList className={"flex flex-wrap -mb-px text-sm font-medium text-center"}>
-                                <div className={"mr-2"}>
-                                    <TabsTrigger value={"Deze week"}
-                                                 className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Deze
-                                        week</TabsTrigger>
-                                </div>
-                                <div className={"mr-2"}>
-                                    <TabsTrigger value={"Te controleren"}
-                                                 className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Te
-                                        controleren</TabsTrigger>
-                                </div>
-                                <div className={"mr-2"}>
-                                    <TabsTrigger value={"Te factureren"}
-                                                 className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Te
-                                        factureren</TabsTrigger>
-                                </div>
-                            </TabsList>
-                        </div>
-                        <TabsContent value={"Deze week"}>
-                            <DashboardReservationsHoldTable reservations={reservationsWeak.data}/>
-                        </TabsContent>
-                        <TabsContent value={"Te controleren"}>
-                            <DashboardReservationsHoldTable reservations={reservationsHold.data}/>
-                        </TabsContent>
-                        <TabsContent value={"Te factureren"}>
-                            <DashboardReservationsHoldTable reservations={reservationsInvoice.data}/>
-                        </TabsContent>
-                    </Tabs>
-                </div>
-                <div className={"p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6"}>
-                    <HoverCard>
-                        <HoverCardTrigger asChild>
-                            <h2 className={"mb-4 pb-2 text-lg font-semibold text-center border-b border-gray-200"}>Te
-                                controleren</h2>
-                        </HoverCardTrigger>
-                        <HoverCardContent>
-                            <p>Dit zijn de te controleren reserveringen.</p>
-                        </HoverCardContent>
-                    </HoverCard>
-                    <p className={"text-6xl font-bold text-green-600 sm:text-8xl text-center"}>{reservationsHold.data.length}</p>
-                </div>
-                <div className={"p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6"}>
-                    <HoverCard>
-                        <HoverCardTrigger asChild>
-                            <h2 className={"mb-4 pb-2 text-lg font-semibold text-center border-b border-gray-200"}>Te
-                                factureren</h2>
-                        </HoverCardTrigger>
-                        <HoverCardContent>
-                            <p>Dit zijn de te factureren reserveringen.</p>
-                        </HoverCardContent>
-                    </HoverCard>
-                    <p className={"text-6xl font-bold text-green-600 sm:text-8xl text-center"}>{reservationsInvoice.data.length}</p>
-                </div>
-                <div
-                    className={"p-4 border border-gray-200 rounded-lg shadow-sm col-span-2 xl:col-span-3 2xl:col-span-4 sm:p-6 xl:row-span-2"}>
-                    <div className="flex items-center justify-between mb-4">
-                        <div className="flex-shrink-0">
-                            <h3
-                                className="text-xl font-bold leading-none text-gray-900 sm:text-2xl dark:text-white">Reserveringen</h3>
-                        </div>
-                        {/*<div
+        <div className={"px-4 py-6"}>
+            <div className={"flex flex-col-reverse lg:flex-row gap-4 min-w-full"}>
+                <div className={"flex flex-col gap-4 flex-auto lg:flex-grow"}>
+                    <div
+                        className={"p-4 border border-gray-200 rounded-lg shadow-sm sm:p-6 lg:max-h-[415px] overflow-y-auto"}>
+                        <Tabs defaultValue={"Deze week"}>
+                            <div className={"mb-4 border-b border-gray-200"}>
+                                <TabsList className={"flex flex-wrap -mb-px text-sm font-medium text-center"}>
+                                    <div className={"mr-2"}>
+                                        <TabsTrigger value={"Deze week"}
+                                                     className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Deze
+                                            week</TabsTrigger>
+                                    </div>
+                                    <div className={"mr-2"}>
+                                        <TabsTrigger value={"Te controleren"}
+                                                     className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Te
+                                            controleren</TabsTrigger>
+                                    </div>
+                                    <div className={"mr-2"}>
+                                        <TabsTrigger value={"Te factureren"}
+                                                     className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Te
+                                            factureren</TabsTrigger>
+                                    </div>
+                                    <div className={"mr-2"}>
+                                        <TabsTrigger value={"Deze maand"}
+                                                     className={"inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:border-green-600 data-[state=active]:border-green-600"}>Deze
+                                            maand
+                                        </TabsTrigger>
+                                    </div>
+                                </TabsList>
+                            </div>
+                            <TabsContent value={"Deze week"}>
+                                <DashboardReservationsHoldTable reservations={reservationsWeak.data}/>
+                            </TabsContent>
+                            <TabsContent value={"Te controleren"}>
+                                <DashboardReservationsHoldTable reservations={reservationsHold.data}/>
+                            </TabsContent>
+                            <TabsContent value={"Te factureren"}>
+                                <DashboardReservationsHoldTable reservations={reservationsInvoice.data}/>
+                            </TabsContent>
+                            <TabsContent value={"Deze maand"}>
+                                <DashboardReservationsHoldTable reservations={reservationsMonth.data}/>
+                            </TabsContent>
+                        </Tabs>
+                    </div>
+                    <div
+                        className={"p-4 border border-gray-200 rounded-lg shadow-sm col-span-2 xl:col-span-3 2xl:col-span-4 sm:p-6 xl:row-span-2 flex-grow"}>
+                        <div className="flex flex-col justify-between mb-4">
+                            <div className="flex-shrink-0">
+                                <h3
+                                    className="text-xl font-bold leading-none text-gray-900 sm:text-2xl dark:text-white">Reserveringen</h3>
+                            </div>
+                            <span>Hier komt nog een grafiek</span>
+                            {/*<div
                             className="flex items-center justify-end flex-1 text-base font-medium text-green-500 dark:text-green-400">
                             12.5%
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
@@ -114,22 +111,67 @@ export default async function page() {
                                       clip-rule="evenodd"></path>
                             </svg>
                         </div>*/}
+                        </div>
                     </div>
                 </div>
-                <div className={"p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6  lg:col-start-5"}>
-                    <HoverCard>
-                        <HoverCardTrigger asChild>
-                            <h2 className={"mb-4 pb-2 text-lg font-semibold text-center border-b border-gray-200"}>
-                                Reserveringen deze week
-                            </h2>
-                        </HoverCardTrigger>
-                        <HoverCardContent>
-                            <p>Dit zijn de reserveringen vanaf maandag deze week.</p>
-                        </HoverCardContent>
-                    </HoverCard>
-                    <p className={"text-6xl font-bold text-green-600 sm:text-8xl text-center"}>{reservationsWeak.data.length}</p>
+                <div className={"flex flex-col gap-4"}>
+                    <div className={"flex flex-row lg:flex-col gap-4 flex-grow"}>
+                        <div className={"p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"}>
+                            <HoverCard>
+                                <HoverCardTrigger asChild>
+                                    <h2 className={"pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200 flex-auto"}>Te
+                                        controleren</h2>
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <p>Dit zijn de te controleren reserveringen.</p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <p className={"text-6xl font-bold text-green-600 sm:text-7xl text-center"}>{reservationsHold.data.length}</p>
+                        </div>
+                        <div className={"p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"}>
+                            <HoverCard>
+                                <HoverCardTrigger asChild>
+                                    <h2 className={"pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200 flex-auto"}>Te
+                                        factureren</h2>
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <p>Dit zijn de te factureren reserveringen.</p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <p className={"text-6xl font-bold text-green-600 sm:text-7xl text-center"}>{reservationsInvoice.data.length}</p>
+                        </div>
+                    </div>
+                    <div className={"flex flex-row lg:flex-col gap-4 flex-grow"}>
+                        <div className={"p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"}>
+                            <HoverCard>
+                                <HoverCardTrigger asChild>
+                                    <h2 className={"pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200"}>
+                                        Reserveringen deze week
+                                    </h2>
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <p>Dit zijn de reserveringen vanaf maandag deze week.</p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <p className={"text-6xl font-bold text-green-600 sm:text-7xl text-center"}>{reservationsWeak.data.length}</p>
+                        </div>
+                        <div className={"p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"}>
+                            <HoverCard>
+                                <HoverCardTrigger asChild>
+                                    <h2 className={"pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200"}>
+                                        Reserveringen deze maand
+                                    </h2>
+                                </HoverCardTrigger>
+                                <HoverCardContent>
+                                    <p>Dit zijn de reserveringen vanaf maandag deze week.</p>
+                                </HoverCardContent>
+                            </HoverCard>
+                            <p className={"text-6xl font-bold text-green-600 sm:text-7xl text-center"}>{reservationsMonth.data.length}</p>
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
     </main>
 }
