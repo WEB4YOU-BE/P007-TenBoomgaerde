979127ff1f522bb324c549264d016950a2e8fc87
Tiebe Deweerdt <tiebe.dw1@gmail.com>
2024-02-27 10:02:01 +0100

    Added the option to add an remark on a reservation after he is made for a customer
    Signed-off-by: Tiebe Deweerdt <tiebe.dw1@gmail.com>

--- components/ui/klant/Reservation.tsx
+++ components/ui/klant/Reservation.tsx
@@ -1,6 +1,11 @@
 import {Badge} from "@/components/ui/Badge";
 import {HoverCard, HoverCardContent, HoverCardTrigger} from "@/components/ui/Hover-card";
 import React from "react";
+import {cn} from "@/lib/utils";
+import {buttonVariants} from "@/components/ui/button";
+import {redirect} from "next/navigation";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
 
 interface reservationProps {
     id: string;
@@ -22,6 +27,16 @@ const Reservation = async (reservation: reservationProps) => {
 
     const voornaam = reservation.users.firstname ?? ""
     const familienaam = reservation.users.lastname ?? ""
+
+    const updateRemark = async (formData: FormData) => {
+        "use server"
+        const remark = formData.get("remark")
+
+        if (remark === null) redirect("/remark")
+
+        const supabase = createServerComponentClient({cookies})
+        await supabase.from("reservations").update({remarks: remark}).eq('id', reservation.id)
+    }
 
     return <div className={"p-4 my-4 border border-gray-200 rounded-lg shadow-sm"}>
         <div className={"grid gap-4 grid-cols-3 grid-rows-4"}>
@@ -67,17 +82,19 @@ const Reservation = async (reservation: reservationProps) => {
                 }
                 {reservation.access_code}
             </div>
-            {reservation.remarks !== undefined && reservation.remarks !== null && (
-                <div className={"sm:row-start-2 col-span-3 sm:col-span-1 sm:row-span-2"}>
-                    <form className={"flex flex-col gap-2 h-full"}>
-                        <label htmlFor={"remark"} className="font-bold">Opmerkingen:</label>
-                        <textarea id={"remark"} name={"remark"} readOnly={true}
-                                  defaultValue={reservation.remarks}
-                                  className={"flex rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}>
+            <div className={"sm:row-start-6 col-span-3 sm:row-span-2"}>
+                <form className={"flex flex-col gap-2 h-full"} action={updateRemark}>
+                    <label htmlFor={"remark"} className="font-bold">Opmerkingen:</label>
+                    <div className={"flex sm:flex-row flex-col justify-between"}>
+                        <textarea id={"remark"} name={"remark"}
+                                  defaultValue={reservation.remarks === null ? "" : reservation.remarks}
+                                  className={"flex w-full max-sm:mb-2 mr-6 rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"}>
                         </textarea>
-                    </form>
-                </div>
-            )}
+                        <button type={"submit"} className={cn(buttonVariants({variant: "green"}), "my-auto")}>Verstuur
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 }
