53fbc36243064a96364bf03baa2a4941c6388e25
Jens Penneman <jenspenneman26@gmail.com>
2024-02-28 15:39:23 +0100

    Changed to sender for the testmail.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/api/cron/emails/route.tsx
+++ app/api/cron/emails/route.tsx
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_SEND_KEY)
 
 export async function GET() {
     await resend.emails.send({
-        from: 'info@vzwtenboomgaerdelichtervelde.be',
+        from: 'VZW Ten Boomgaerde Lichtervelde <info@vzwtenboomgaerdelichtervelde.be>',
         to: 'jenspenneman26@gmail.com',
         subject: 'Bevestiging reservatie bij Ten Boomgaerde',
         react: TestMail(),
