a8cddbd73b6a30063af3954f22d968fd13932468
Jens Penneman <jenspenneman26@gmail.com>
2024-02-28 15:19:42 +0100

    Enabled a cron job which just sends an example mail to Jens Penneman.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

---
+++ app/api/cron/emails/route.tsx
@@ -0,0 +1,19 @@
+import {Resend} from "resend";
+
+import TestMail from "@/emails/test"
+import {NextResponse} from "next/server";
+
+const resend = new Resend(process.env.RESEND_SEND_KEY)
+
+export async function GET() {
+    await resend.emails.send({
+        from: 'info@vzwtenboomgaerdelichtervelde.be',
+        to: 'jenspenneman26@gmail.com',
+        subject: 'Bevestiging reservatie bij Ten Boomgaerde',
+        react: TestMail(),
+    });
+
+    return NextResponse.json({
+        status: "OK"
+    })
+}
