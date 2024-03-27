f40dde36d7a49fd6a1a4d633bd709b587bde6aea
Jens Penneman <jenspenneman26@gmail.com>
2024-03-27 15:51:57 +0100

    Updated build failures and rebase errors.
    Signed-off-by: Jens Penneman <jenspenneman26@gmail.com>

--- app/api/cron/emails/route.tsx
+++ app/api/cron/emails/route.tsx
@@ -1,16 +1,17 @@
 import {Resend} from "resend";
 
 import TestMail from "@/emails/reservatie-bevestiging"
+import {NextResponse} from "next/server";
 
 const resend = new Resend(process.env.RESEND_SEND_KEY)
 
 export async function GET() {
-
-
     await resend.emails.send({
         from: 'VZW Ten Boomgaerde Lichtervelde <info@vzwtenboomgaerdelichtervelde.be>',
         to: 'jenspenneman26@gmail.com',
         subject: 'Bevestiging reservatie bij Ten Boomgaerde',
         react: TestMail({voornaam: "Jens"}),
     });
+
+    return NextResponse.json({})
 }
