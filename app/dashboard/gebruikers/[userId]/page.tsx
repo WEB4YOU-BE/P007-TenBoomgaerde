019781542ecffa7c342c83fdc7c3cbf72ee03ae5
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-11 16:26:44 +0200

    Folder reset

--- app/dashboard/gebruikers/[userId]/page.tsx
+++
@@ -1,9 +0,0 @@
-import InfoUserForm from "@/components/business/users/info-users-form";
-
-export default async function page({params}: { params: { userId: string } }) {
-    return <main className={"mx-auto p-4 lg:px-8 flex flex-col gap-4"}>
-        <h1 className={"text-4xl font-extrabold tracking-tight lg:text-5xl md:flex-grow whitespace-nowrap truncate text-center"}>Gebruiker
-            bekijken</h1>
-        <InfoUserForm id={params.userId}/>
-    </main>
-}
