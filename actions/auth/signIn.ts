3445c3752cba0095a9f3940633e3764822cca882
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-12 20:35:55 +0200

    Made arrow functions for the auth actions

--- actions/auth/signIn.ts
+++ actions/auth/signIn.ts
@@ -3,11 +3,11 @@
 import { createClient } from "@/utils/supabase/server";
 import type { SignInWithPasswordCredentials } from "@supabase/supabase-js";
 
-export async function SignInWithCredentials(
+export const SignInWithCredentials = async (
   credentials: SignInWithPasswordCredentials,
-) {
+) => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.signInWithPassword(credentials);
   if (error) throw error;
   return data;
-}
+};
--- actions/auth/signUp.ts
+++ actions/auth/signUp.ts
@@ -1,11 +1,11 @@
 import { createClient } from "@/utils/supabase/server";
 import { SignUpWithPasswordCredentials } from "@supabase/supabase-js";
 
-export async function SignUpWithCredentials(
+export const SignUpWithCredentials = async (
   credentials: SignUpWithPasswordCredentials,
-) {
+) => {
   const supabase = createClient();
   const { data, error } = await supabase.auth.signUp(credentials);
   if (error) throw error;
   return data;
-}
+};
