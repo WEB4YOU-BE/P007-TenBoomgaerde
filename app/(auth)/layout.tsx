4c652013bd9169cab4a584ae507151c05a2c538e
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-13 12:20:17 +0200

    Made the company name clickable when signing in

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,20 @@
 import { ReactNode } from "react";
 
 import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/utils/tailwindcss/twMergeCN";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Layout({ children }: { children: ReactNode }) {
     return <>
         <div className="container relative h-[100vh] flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
-                <div className="relative z-20 flex items-center text-lg font-medium">
+                <Link href={"/"} className={cn(buttonVariants({ variant: "link" }), "relative z-20 flex items-center ps-0 justify-start text-lg font-medium w-fit")}>
                     <Image src={"/images/Logo Ten Boomgaerde.PNG"} alt={"Logo"} width={40} height={40}
                         className={"aspect-square mr-2 h-8 w-8 rounded-full"} />
                     VZW Ten Boomgaerde Lichtervelde
-                </div>
+                </Link>
                 <div className="relative z-20 mt-auto">
                     <blockquote className="space-y-2">
                         <p className="text-lg text-balance">
