b877e048213adf01553686edf517ab18909e140f
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-17 17:57:28 +0200

    Added Speed Insights to the project

--- components/Hosts.tsx
+++ components/Hosts.tsx
@@ -1,10 +1,13 @@
 import { Toaster as ShadCNToaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 const Hosts = () => {
     return <>
         <ShadCNToaster />
         <SonnerToaster />
+        <SpeedInsights />
     </>
 }
 
