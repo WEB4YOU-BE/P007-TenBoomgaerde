45a5c6b745ba5dc5c17e6a19913349d7d80f90f4
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-17 19:32:03 +0200

    Added Google Tag Manager and ENV types

--- components/Hosts.tsx
+++ components/Hosts.tsx
@@ -4,12 +4,15 @@ import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
 
+import { GoogleTagManager } from '@next/third-parties/google'
+
 const Hosts = () => {
     return <>
         <ShadCNToaster />
         <SonnerToaster />
         <SpeedInsights />
         <Analytics />
+        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
     </>
 }
 
---
+++ types/env/googleTagManager.ts
@@ -0,0 +1,5 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID: string
+    }
+}
