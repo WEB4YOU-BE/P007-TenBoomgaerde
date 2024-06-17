89d8e6a0314af32d429e2612720e6a68d8c20f3b
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-17 17:51:19 +0200

    Added OpenTelemetry to the project

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 import withVercelToolbar from "@vercel/toolbar/plugins/next";
 
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    experimental: {
+        instrumentationHook: true,
+    }
+};
 
 export default withVercelToolbar()(nextConfig);
