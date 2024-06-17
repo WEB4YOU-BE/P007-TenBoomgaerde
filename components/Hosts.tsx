4abace18b9bcd3d58cef138a6ab269ea30153ff4
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-17 18:02:02 +0200

    Added Vercel Analytics

--- components/Hosts.tsx
+++ components/Hosts.tsx
@@ -2,12 +2,14 @@ import { Toaster as ShadCNToaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const Hosts = () => {
     return <>
         <ShadCNToaster />
         <SonnerToaster />
         <SpeedInsights />
+        <Analytics />
     </>
 }
 
