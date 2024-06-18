b9bd9b6cb25f59f530ed8a283af777fe761a8453
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-18 17:03:49 +0200

    Added basic metadata to the root

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "VZW Ten Boomgaerde Lichtervelde",
+  alternates: {
+    canonical: "/",
+  },
+};
+
 export default async function Page() {
-  return <>
-  </>
+  return <></>;
 }
