822a54684437a2086a4b3dff2121afa52122ff71
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-19 19:31:32 +0200

    Re-added the server side rendered client for Supabase

---
+++ utils/supabase/server.ts
@@ -0,0 +1,22 @@
+import { cookies } from "next/headers";
+import { type CookieOptions, createServerClient } from "@supabase/ssr";
+
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get: (key: string) => cookieStore.get(key)?.value,
+        set: (key: string, value: string, options: CookieOptions) => {
+          cookieStore.set({ name: key, value, ...options });
+        },
+        remove: (key: string, options: CookieOptions) => {
+          cookieStore.set({ name: key, value: "", ...options });
+        },
+      },
+    },
+  );
+}
