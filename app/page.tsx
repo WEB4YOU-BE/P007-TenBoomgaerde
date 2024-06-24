63c4581989a6a70cf7c9bae1a04e0c6cfe8b09f5
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-24 20:06:13 +0200

    Working everything auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { createClient } from "@/utils/supabase/server";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "VZW Ten Boomgaerde Lichtervelde",
@@ -8,5 +11,23 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  return <></>;
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.getUser();
+
+  return (
+    <>
+      <Card className="w-fit m-10">
+        <CardHeader>
+          <CardTitle>Gebruiker</CardTitle>
+        </CardHeader>
+        <CardContent>user: {data.user?.email}</CardContent>
+        <CardContent>error: {JSON.stringify(error, null, 2)}</CardContent>
+        {!data?.user && (
+          <CardFooter>
+            <Link href={"/authentication/"}>Sign in</Link>
+          </CardFooter>
+        )}
+      </Card>
+    </>
+  );
 }
--- types/env/supabase.ts
+++ types/env/supabase.ts
@@ -1,6 +1,9 @@
 declare namespace NodeJS {
-    export interface ProcessEnv {
-        SUPABASE_URL: string;
-        SUPABASE_ANON_KEY: string;
-    }
+  export interface ProcessEnv {
+    SUPABASE_URL: string;
+    SUPABASE_ANON_KEY: string;
+
+    NEXT_PUBLIC_SUPABASE_URL: string;
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
+  }
 }
