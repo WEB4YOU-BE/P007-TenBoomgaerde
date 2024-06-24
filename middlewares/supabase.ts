865d8e235f12ea122eca9cb8ada092c3b7cc68af
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-24 14:44:38 +0200

    Simplyfied the if statement for the Supabase Middleware

--- middlewares/supabase.ts
+++ middlewares/supabase.ts
@@ -15,64 +15,62 @@ export const withSupabaseAuth: MiddlewareFactory = (next) => {
         "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
       ].some((regex) => pathname.match(regex))
     )
-      {
-        let response = NextResponse.next({
-            request: {
-              headers: request.headers,
-            },
-          });
-        
-          const supabase = createServerClient(
-            process.env.SUPABASE_URL!,
-            process.env.SUPABASE_ANON_KEY!,
-            {
-              cookies: {
-                get(name: string) {
-                  return request.cookies.get(name)?.value;
-                },
-                set(name: string, value: string, options: CookieOptions) {
-                  request.cookies.set({
-                    name,
-                    value,
-                    ...options,
-                  });
-                  response = NextResponse.next({
-                    request: {
-                      headers: request.headers,
-                    },
-                  });
-                  response.cookies.set({
-                    name,
-                    value,
-                    ...options,
-                  });
-                },
-                remove(name: string, options: CookieOptions) {
-                  request.cookies.set({
-                    name,
-                    value: "",
-                    ...options,
-                  });
-                  response = NextResponse.next({
-                    request: {
-                      headers: request.headers,
-                    },
-                  });
-                  response.cookies.set({
-                    name,
-                    value: "",
-                    ...options,
-                  });
-                },
-              },
-            },
-          );
-        
-          await supabase.auth.getUser();
-        
-          return response;
-      }
+      return next(request, _next);
 
-    return next(request, _next);
+    let response = NextResponse.next({
+      request: {
+        headers: request.headers,
+      },
+    });
+
+    const supabase = createServerClient(
+      process.env.SUPABASE_URL!,
+      process.env.SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          get(name: string) {
+            return request.cookies.get(name)?.value;
+          },
+          set(name: string, value: string, options: CookieOptions) {
+            request.cookies.set({
+              name,
+              value,
+              ...options,
+            });
+            response = NextResponse.next({
+              request: {
+                headers: request.headers,
+              },
+            });
+            response.cookies.set({
+              name,
+              value,
+              ...options,
+            });
+          },
+          remove(name: string, options: CookieOptions) {
+            request.cookies.set({
+              name,
+              value: "",
+              ...options,
+            });
+            response = NextResponse.next({
+              request: {
+                headers: request.headers,
+              },
+            });
+            response.cookies.set({
+              name,
+              value: "",
+              ...options,
+            });
+          },
+        },
+      },
+    );
+
+    await supabase.auth.getUser();
+
+    return response;
   };
 };
