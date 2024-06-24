013216e504b7293410c53604e539c12ed19182f9
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-24 15:34:13 +0200

    Added rich colors for the sonner toasts

--- components/Hosts.tsx
+++ components/Hosts.tsx
@@ -1,20 +1,22 @@
 import { Toaster as ShadCNToaster } from "@/components/ui/toaster";
 import { Toaster as SonnerToaster } from "@/components/ui/sonner";
 
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
-import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const Hosts = () => {
-    return <>
-        <ShadCNToaster />
-        <SonnerToaster />
-        <SpeedInsights />
-        <Analytics />
-        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
-        <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
+  return (
+    <>
+      <ShadCNToaster />
+      <SonnerToaster richColors />
+      <SpeedInsights />
+      <Analytics />
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+      <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID} />
     </>
-}
+  );
+};
 
-export default Hosts
+export default Hosts;
