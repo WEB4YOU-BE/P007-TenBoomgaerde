2fb18a3a159c853fd38ce15b7fa08688da44d989
Jens Penneman <jens.penneman@web-4-you.be>
2024-07-01 00:26:20 +0200

    Added navigation UI to the account section

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,7 +1,15 @@
 import { Separator } from "@/components/atoms/separator";
+import { AccountNavigation } from "@/components/ui/account-navigation";
 import { ReactNode } from "react";
 
 export default async function Layout({ children }: { children: ReactNode }) {
+  const navigationItems = [
+    {
+      title: "Account",
+      href: "/account/",
+    },
+  ];
+
   return (
     <div className="container mx-auto space-y-6 p-10 pb-16">
       <div className="space-y-0.5">
@@ -13,7 +21,7 @@ export default async function Layout({ children }: { children: ReactNode }) {
       <Separator className="my-6" />
       <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
         <aside className="-mx-4 lg:w-1/5">
-          {/* <SidebarNav items={sidebarNavItems} /> */}
+          <AccountNavigation items={navigationItems} />
         </aside>
         <div className="flex-1 lg:max-w-2xl">{children}</div>
       </div>
