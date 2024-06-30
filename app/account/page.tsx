5c04f2c6a71e04dc9f9e1728ca7aef79598007f0
Jens Penneman <jens.penneman@web-4-you.be>
2024-07-01 00:28:31 +0200

    Added basic metadata to the account page

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Account",
+  alternates: {
+    canonical: "/account/",
+  },
+};
+
 export default async function Page() {
   return <></>;
 }
