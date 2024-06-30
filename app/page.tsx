26881a2834707bac48c7f42cada48577e460d422
Jens Penneman <jens.penneman@web-4-you.be>
2024-06-30 22:12:44 +0200

    Removed the auth card on login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { createClient } from "@/utils/supabase/server";
 import { Metadata } from "next";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "VZW Ten Boomgaerde Lichtervelde",
@@ -11,23 +8,5 @@ export const metadata: Metadata = {
 };
 
 export default async function Page() {
-  const supabase = createClient();
-  const { data, error } = await supabase.auth.getUser();
-
-  return (
-    <>
-      <Card className="w-fit m-10">
-        <CardHeader>
-          <CardTitle>Gebruiker</CardTitle>
-        </CardHeader>
-        <CardContent>user: {data.user?.email}</CardContent>
-        <CardContent>error: {JSON.stringify(error, null, 2)}</CardContent>
-        
-          <CardFooter>
-            <Link href={"/authentication/"}>Sign in</Link>
-          </CardFooter>
-        
-      </Card>
-    </>
-  );
+  return <></>;
 }
