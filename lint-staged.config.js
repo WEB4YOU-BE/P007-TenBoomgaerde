5409028a4d66a2199fe76944d03dfe63121d3966
Jens Penneman <jens.penneman@web-4-you.be>
2024-08-25 23:16:12 +0200

    feat: added prettier to the perfect code mix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 const config = {
-  "*.{js,jsx,ts,tsx}": ["eslint --fix", "eslint"],
-  "*.{json,md,yml}": [],
+    "*.{js,jsx,ts,tsx}": ["npm run -s format:fix", "eslint --fix", "eslint"],
+    "*.{json,md,yml}": ["npm run -s format:fix"],
 };
 export default config;
