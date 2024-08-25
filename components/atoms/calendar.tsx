346bfc849bd477490387425701e0e038df3542c1
Jens Penneman <jens.penneman@web-4-you.be>
2024-08-26 00:57:36 +0200

    feat: deleted Vercel package and updated react-day-picker

--- components/atoms/calendar.tsx
+++ components/atoms/calendar.tsx
@@ -54,10 +54,12 @@ function Calendar({
                 ...classNames,
             }}
             components={{
-                IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-                IconRight: ({ ...props }) => (
-                    <ChevronRight className="h-4 w-4" />
-                ),
+                Chevron: ({ orientation, ...props }) =>
+                    orientation === "left" ? (
+                        <ChevronLeft {...props} />
+                    ) : (
+                        <ChevronRight {...props} />
+                    ),
             }}
             {...props}
         />
