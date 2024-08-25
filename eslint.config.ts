1a57b582f08a9e9f4f93d3519babd8ebddcf9dee
Jens Penneman <jens.penneman@web-4-you.be>
2024-08-25 23:41:35 +0200

    feat: added prettier to the ts eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,15 +3,23 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+
 export default tseslint.config(
     eslint.configs.recommended,
     ...tseslint.configs.strictTypeChecked,
+    prettierRecommended,
     {
         languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
             parserOptions: {
                 projectService: true,
                 tsconfigRootDir: import.meta.dirname,
             },
+        },
+        rules: {
+            "prettier/prettier": "error",
         },
     }
 );
