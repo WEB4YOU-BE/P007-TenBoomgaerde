a554c2577bd1f544a1522d4d495a0bcd8e33048b
Jens Penneman <jens.penneman@web-4-you.be>
2024-08-28 22:04:37 +0200

    chore: removed console log in middleware stack

--- middlewares/stack.ts
+++ middlewares/stack.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server";
 import type { Stack } from "@/types/middleware/stack";
 
 const stack: Stack = async ({ plugins, request, event }) => {
-    console.log("Stack middleware is called");
     return NextResponse.next();
 };
 export default stack;
