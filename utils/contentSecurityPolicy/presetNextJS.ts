0927a660309d69d0daa4f9848d0460d82578cd4e
Jens Penneman <jens.penneman@web-4-you.be>
2024-08-31 16:15:13 +0200

    fix: added itself to all the NextJS directives

--- utils/contentSecurityPolicy/presetNextJS.ts
+++ utils/contentSecurityPolicy/presetNextJS.ts
@@ -4,6 +4,12 @@ const preset: Partial<CSPDirectives> = {
     "default-src": [SELF],
 
     "style-src": [SELF, UNSAFE_INLINE],
+    "font-src": [SELF],
     "script-src": [SELF, UNSAFE_INLINE],
+
+    "img-src": [SELF],
+    "media-src": [SELF],
+
+    "connect-src": [SELF],
 };
 export default preset;
