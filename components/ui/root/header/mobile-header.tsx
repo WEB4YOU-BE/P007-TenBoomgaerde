b521e320cb535ff653a540c7245edcb2a8d14047
Jens Penneman <jens.penneman@web-4-you.be>
2024-10-07 21:38:44 +0200

    feat: added alert to mobile banner, redirecting to the desktop version

--- components/ui/root/header/mobile-header.tsx
+++ components/ui/root/header/mobile-header.tsx
@@ -1,3 +1,4 @@
+import { Alert } from "@/components/atoms/alert";
 import { cn } from "@/utils/tailwindcss/mergeClassNames";
 import { forwardRef, HTMLAttributes } from "react";
 import React from "react";
@@ -8,8 +9,15 @@ interface MobileHeaderProps extends HTMLAttributes<HTMLDivElement> {
 const MobileHeader = forwardRef<HTMLDivElement, MobileHeaderProps>(
     ({ className, ...props }, ref) => {
         return (
-            <div className={cn("", className)} ref={ref} {...props}>
-                {" "}
+            <div className={cn("p-2", className)} ref={ref} {...props}>
+                <Alert
+                    className="bg-destructive-foreground text-balance"
+                    variant={"destructive"}
+                >
+                    We zijn momenteel bezig met het ontwikkelen van de mobiele
+                    website. Probeer het later nog eens of bezoek de website op
+                    een desktop.
+                </Alert>
             </div>
         );
     }
