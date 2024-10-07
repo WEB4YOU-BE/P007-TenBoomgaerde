175af94133e3d855e05e8f09d2956d54d031b5f5
Jens Penneman <jens.penneman@web-4-you.be>
2024-10-07 22:13:43 +0200

    feat: added the main dashboard page

---
+++ app/dashboard/actions.ts
@@ -0,0 +1,86 @@
+"use server";
+
+import createClient from "@/utils/supabase/server";
+import { add } from "date-fns";
+
+export const fetchReservationsToBeReviewed = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase
+        .from("reservations")
+        .select()
+        .eq("status", "in afwachting");
+    if (error) throw error;
+    return data;
+};
+export const fetchReservationsToBeInvoiced = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase
+        .from("reservations")
+        .select()
+        .eq("status", "goedgekeurd")
+        .eq("gefactureerd", "FALSE");
+    if (error) throw error;
+    return data;
+};
+
+export const fetchReservationsToBeReviewedCount = async () => {
+    const supabase = createClient();
+    const { count, error } = await supabase
+        .from("reservations")
+        .select("id", { count: "exact" })
+        .eq("status", "in afwachting");
+    if (error) throw error;
+    return count ?? 0;
+};
+export const fetchReservationsToBeInvoicedCount = async () => {
+    const supabase = createClient();
+    const { count, error } = await supabase
+        .from("reservations")
+        .select("id", { count: "exact" })
+        .eq("status", "goedgekeurd")
+        .eq("gefactureerd", "FALSE");
+    if (error) throw error;
+    return count ?? 0;
+};
+
+export const fetchReservationsThisWeek = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase
+        .from("reservations")
+        .select()
+        .gte("start_date", new Date().toISOString())
+        .lte("end_date", add(new Date(), { days: 7 }).toISOString());
+    if (error) throw error;
+    return data;
+};
+export const fetchReservationsThisMonth = async () => {
+    const supabase = createClient();
+    const { data, error } = await supabase
+        .from("reservations")
+        .select()
+        .gte("start_date", new Date().toISOString())
+        .lte("end_date", add(new Date(), { months: 1 }).toISOString());
+    if (error) throw error;
+    return data;
+};
+
+export const fetchReservationsThisWeekCount = async () => {
+    const supabase = createClient();
+    const { count, error } = await supabase
+        .from("reservations")
+        .select("id", { count: "exact" })
+        .gte("start_date", new Date().toISOString())
+        .lte("end_date", add(new Date(), { days: 7 }).toISOString());
+    if (error) throw error;
+    return count ?? 0;
+};
+export const fetchReservationsThisMonthCount = async () => {
+    const supabase = createClient();
+    const { count, error } = await supabase
+        .from("reservations")
+        .select("id", { count: "exact" })
+        .gte("start_date", new Date().toISOString())
+        .lte("end_date", add(new Date(), { months: 1 }).toISOString());
+    if (error) throw error;
+    return count ?? 0;
+};
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,186 @@
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from "@/components/atoms/hover-card";
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/atoms/tabs";
 import React from "react";
 
+import {
+    fetchReservationsThisMonth,
+    fetchReservationsThisMonthCount,
+    fetchReservationsThisWeek,
+    fetchReservationsThisWeekCount,
+    fetchReservationsToBeInvoiced,
+    fetchReservationsToBeInvoicedCount,
+    fetchReservationsToBeReviewed,
+    fetchReservationsToBeReviewedCount,
+} from "./actions";
+import { columns, DataTable } from "./reservations/table";
+
 export default async function Page() {
-    return <>page</>;
+    const reservationsThisWeek = await fetchReservationsThisWeek();
+    const reservationsThisMonth = await fetchReservationsThisMonth();
+    const reservationsToBeReviewed = await fetchReservationsToBeReviewed();
+    const reservationsToBeInvoiced = await fetchReservationsToBeInvoiced();
+
+    return (
+        <div className={"flex flex-row gap-2 h-full"}>
+            <Tabs className="grow" defaultValue="Deze week">
+                <TabsList className={"flex flex-wrap"}>
+                    <TabsTrigger value={"Deze week"}>Deze week</TabsTrigger>
+                    <TabsTrigger value={"Deze maand"}>Deze maand</TabsTrigger>
+                    <TabsTrigger value={"Te controleren"}>
+                        Te controleren
+                    </TabsTrigger>
+                    <TabsTrigger value={"Te factureren"}>
+                        Te factureren
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="Deze week">
+                    <DataTable columns={columns} data={reservationsThisWeek} />
+                </TabsContent>
+                <TabsContent value="Deze maand">
+                    <DataTable columns={columns} data={reservationsThisMonth} />
+                </TabsContent>
+                <TabsContent value="Te controleren">
+                    <DataTable
+                        columns={columns}
+                        data={reservationsToBeReviewed}
+                    />
+                </TabsContent>
+                <TabsContent value="Te factureren">
+                    <DataTable
+                        columns={columns}
+                        data={reservationsToBeInvoiced}
+                    />
+                </TabsContent>
+            </Tabs>
+            <div className={"flex flex-col gap-4 h-full"}>
+                <div className={"flex flex-row lg:flex-col gap-4 flex-grow"}>
+                    <div
+                        className={
+                            "p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"
+                        }
+                    >
+                        <HoverCard>
+                            <HoverCardTrigger asChild>
+                                <h2
+                                    className={
+                                        "pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200 flex-auto"
+                                    }
+                                >
+                                    Te controleren
+                                </h2>
+                            </HoverCardTrigger>
+                            <HoverCardContent>
+                                <p>Dit zijn de te controleren reserveringen.</p>
+                            </HoverCardContent>
+                        </HoverCard>
+                        <p
+                            className={
+                                "text-6xl font-bold text-green-600 sm:text-7xl text-center"
+                            }
+                        >
+                            {fetchReservationsToBeReviewedCount()}
+                        </p>
+                    </div>
+                    <div
+                        className={
+                            "p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"
+                        }
+                    >
+                        <HoverCard>
+                            <HoverCardTrigger asChild>
+                                <h2
+                                    className={
+                                        "pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200 flex-auto"
+                                    }
+                                >
+                                    Te factureren
+                                </h2>
+                            </HoverCardTrigger>
+                            <HoverCardContent>
+                                <p>Dit zijn de te factureren reserveringen.</p>
+                            </HoverCardContent>
+                        </HoverCard>
+                        <p
+                            className={
+                                "text-6xl font-bold text-green-600 sm:text-7xl text-center"
+                            }
+                        >
+                            {fetchReservationsToBeInvoicedCount()}
+                        </p>
+                    </div>
+                </div>
+                <div className={"flex flex-row lg:flex-col gap-4 flex-grow"}>
+                    <div
+                        className={
+                            "p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"
+                        }
+                    >
+                        <HoverCard>
+                            <HoverCardTrigger asChild>
+                                <h2
+                                    className={
+                                        "pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200"
+                                    }
+                                >
+                                    Reserveringen deze week
+                                </h2>
+                            </HoverCardTrigger>
+                            <HoverCardContent>
+                                <p>
+                                    Dit zijn de reserveringen vanaf maandag deze
+                                    week.
+                                </p>
+                            </HoverCardContent>
+                        </HoverCard>
+                        <p
+                            className={
+                                "text-6xl font-bold text-green-600 sm:text-7xl text-center"
+                            }
+                        >
+                            {fetchReservationsThisWeekCount()}
+                        </p>
+                    </div>
+                    <div
+                        className={
+                            "p-2 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 flex-1"
+                        }
+                    >
+                        <HoverCard>
+                            <HoverCardTrigger asChild>
+                                <h2
+                                    className={
+                                        "pb-2 mb-4 text-lg font-semibold text-center border-b border-gray-200"
+                                    }
+                                >
+                                    Reserveringen deze maand
+                                </h2>
+                            </HoverCardTrigger>
+                            <HoverCardContent>
+                                <p>
+                                    Dit zijn de reserveringen vanaf maandag deze
+                                    week.
+                                </p>
+                            </HoverCardContent>
+                        </HoverCard>
+                        <p
+                            className={
+                                "text-6xl font-bold text-green-600 sm:text-7xl text-center"
+                            }
+                        >
+                            {fetchReservationsThisMonthCount()}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
