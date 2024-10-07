bb45fbf3d3941bed599fb0fb3998474b9284ad86
Jens Penneman <jens.penneman@web-4-you.be>
2024-10-07 20:42:29 +0200

    revert: removed the CSP-plugin from middleware

--- middlewares/plugins/index.ts
+++ middlewares/plugins/index.ts
@@ -1,4 +1,3 @@
 export { default as pluginDefault } from "./_default";
 
-export { default as pluginContentSecurityPolicy } from "./pluginContentSecurityPolicy";
 export { default as pluginSupabase } from "./pluginSupabase";
