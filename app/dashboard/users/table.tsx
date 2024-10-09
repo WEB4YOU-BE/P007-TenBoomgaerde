d5de925f77aa50ba4579c1ca53c5e5bda499d2d7
Jens Penneman <jens.penneman@web-4-you.be>
2024-10-09 20:45:29 +0200

    chore: upgrade all dependecies in range

--- app/dashboard/users/table.tsx
+++ app/dashboard/users/table.tsx
@@ -1,25 +1,9 @@
 "use client";
 
 import { Button } from "@/components/atoms/button";
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/atoms/table";
 import { Tables } from "@/types/supabase/database";
-import {
-    ColumnDef,
-    flexRender,
-    getCoreRowModel,
-    getSortedRowModel,
-    type SortingState,
-    useReactTable,
-} from "@tanstack/react-table";
+import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
-import { useState } from "react";
 import React from "react";
 
 export const columns: ColumnDef<Tables<"users">>[] = [
@@ -121,77 +105,4 @@ export const columns: ColumnDef<Tables<"users">>[] = [
     },
 ];
 
-interface TableProps<TData, TValue> {
-    columns: ColumnDef<TData, TValue>[];
-    data: TData[];
-}
-
-export function DataTable<TData, TValue>({
-    columns,
-    data,
-}: TableProps<TData, TValue>) {
-    const [sorting, setSorting] = useState<SortingState>([]);
-
-    const table = useReactTable({
-        columns,
-        data,
-        getCoreRowModel: getCoreRowModel(),
-        getSortedRowModel: getSortedRowModel(),
-        onSortingChange: setSorting,
-        state: { sorting },
-    });
-
-    return (
-        <div className="rounded-md border">
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                  header.column.columnDef
-                                                      .header,
-                                                  header.getContext()
-                                              )}
-                                    </TableHead>
-                                );
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows?.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                data-state={row.getIsSelected() && "selected"}
-                                key={row.id}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(
-                                            cell.column.columnDef.cell,
-                                            cell.getContext()
-                                        )}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell
-                                className="h-24 text-center"
-                                colSpan={columns.length}
-                            >
-                                Geen resultaten.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
-        </div>
-    );
-}
+export { DataTable } from "@/components/ui/dashboard/dataTable";
