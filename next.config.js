0e3a407d13357324f7e2e869c483b1db1469effe
Jens Penneman <jens.penneman@web-4-you.be>
2024-10-29 21:45:30 +0100

    fix: removed unrecognized prop instrumentationHook from Next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ import withVercelToolbar from "@vercel/toolbar/plugins/next";
 
 /** @type {import("next").NextConfig} */
 const nextConfig = {
-    experimental: {
-        instrumentationHook: true,
-    },
     trailingSlash: true,
 };
 
