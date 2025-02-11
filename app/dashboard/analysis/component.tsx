3782baca5390c323921994c713df6493e5a61847
Jens Penneman <jens.penneman@web-4-you.be>
2025-02-11 20:58:07 +0100

    feat: some WIP I don't remember

---
+++ app/dashboard/analysis/component.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable perfectionist/sort-objects */
+"use client";
+
+import { buttonVariants } from "@/components/atoms/button";
+import { Tables } from "@/types/supabase/database";
+import * as XLSX from "@e965/xlsx";
+import Link from "next/link";
+import React, { FC } from "react";
+
+import { getHallById } from "../reservations/_tableCells/actions";
+
+interface Props {
+    reservations: Tables<"reservations">[];
+    text: string;
+}
+const DownloadComponent: FC<Props> = ({ reservations, text }) => {
+    const handleDownloadExcel = async () => {
+        const rows = reservations.map(async (reservation) => {
+            return {
+                Reservatienummer:
+                    reservation.reservation_year.substring(0, 4) +
+                    "-" +
+                    reservation.reservation_number,
+                Startdatum: reservation.start_date
+                    ? new Date(reservation.start_date).toLocaleDateString(
+                          "nl-NL",
+                          {
+                              day: "2-digit",
+                              month: "2-digit",
+                              year: "numeric",
+                          }
+                      )
+                    : "Onbekend",
+                Einddatum: reservation.end_date
+                    ? new Date(reservation.end_date).toLocaleDateString(
+                          "nl-NL",
+                          {
+                              day: "2-digit",
+                              month: "2-digit",
+                              year: "numeric",
+                          }
+                      )
+                    : "Onbekend",
+                Startuur: reservation.start_hour,
+                Einduur: reservation.end_hour,
+                Zaal: await getHallById(reservation.room_id || ""),
+                Reserveerder: reservation.user_id,
+                Organisatie: reservation.organizations_id,
+                Toegangscode:
+                    reservation.access_code === null
+                        ? "Onbekend"
+                        : reservation.access_code,
+                Status: reservation.status,
+                Gefactureerd: reservation.gefactureerd,
+            };
+        });
+
+        const workbook = XLSX.utils.book_new();
+        const worksheet = XLSX.utils.json_to_sheet(rows);
+
+        XLSX.utils.book_append_sheet(workbook, worksheet, "Reservations");
+
+        // customize header names
+        XLSX.utils.sheet_add_aoa(worksheet, [
+            [
+                "Reservatienummer",
+                "Start datum",
+                "Eind datum",
+                "Start uur",
+                "Eind uur",
+                "Zaal",
+                "Reserveerder",
+                "Organisatie",
+                "Toegangscode",
+                "Status",
+                "Gefactureerd",
+            ],
+        ]);
+
+        XLSX.writeFile(workbook, "Reservaties.xlsx", { compression: true });
+    };
+
+    return (
+        <div className="rounded border p-4 flex flex-row gap-2 justify-between items-center">
+            <h2 className="font-semibold text-xl">{text}</h2>
+            <div aria-label="actions" className="flex flex-row gap-2">
+                <Link
+                    className={buttonVariants({ variant: "ghost" })}
+                    href="/dashboard/reservations/"
+                >
+                    Bekijk alle reservaties
+                </Link>
+                <button
+                    className={buttonVariants()}
+                    onClick={handleDownloadExcel}
+                >
+                    Excel
+                </button>
+                <button className={buttonVariants()}>PDF</button>
+            </div>
+        </div>
+    );
+};
+export default DownloadComponent;
--- app/dashboard/analysis/loading.tsx
+++
@@ -1,6 +0,0 @@
-import { Skeleton } from "@/components/atoms/skeleton";
-import React from "react";
-
-export default async function Loading() {
-    return <Skeleton className="h-full w-full" />;
-}
