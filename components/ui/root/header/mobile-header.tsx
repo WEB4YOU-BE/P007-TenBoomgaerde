822e1811695ffeb99a51f283ce9c0ef4192a7a98
Jens Penneman <jens.penneman@web-4-you.be>
2025-04-23 23:24:33 +0200

    refactor: converted the mobile header to a React 19 component

--- components/ui/root/header/mobile-header.tsx
+++ components/ui/root/header/mobile-header.tsx
@@ -1,27 +1,21 @@
 import Alert from "@/components/atoms/Alert";
 import { cn } from "@/utils/tailwindcss/mergeClassNames";
-import { forwardRef, HTMLAttributes } from "react";
+import { ComponentPropsWithRef, FC } from "react";
 import React from "react";
 
-interface MobileHeaderProps extends HTMLAttributes<HTMLDivElement> {
+interface MobileHeaderProps extends ComponentPropsWithRef<"div"> {
     items: NavigationItem[];
 }
-const MobileHeader = forwardRef<HTMLDivElement, MobileHeaderProps>(
-    ({ className, ...props }, ref) => {
-        return (
-            <div className={cn("p-2", className)} ref={ref} {...props}>
-                <Alert
-                    className="bg-destructive-foreground text-balance"
-                    variant={"destructive"}
-                >
-                    We zijn momenteel bezig met het ontwikkelen van de mobiele
-                    website. Probeer het later nog eens of bezoek de website op
-                    een desktop.
-                </Alert>
-            </div>
-        );
-    }
+const MobileHeader: FC<MobileHeaderProps> = ({ className, ref, ...props }) => (
+    <div className={cn("p-2", className)} ref={ref} {...props}>
+        <Alert
+            className="bg-destructive-foreground text-balance"
+            variant={"destructive"}
+        >
+            We zijn momenteel bezig met het ontwikkelen van de mobiele website.
+            Probeer het later nog eens of bezoek de website op een desktop.
+        </Alert>
+    </div>
 );
-MobileHeader.displayName = "Main site header for mobile";
 
 export default MobileHeader;
