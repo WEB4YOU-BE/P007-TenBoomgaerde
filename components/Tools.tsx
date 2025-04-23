18d161b86076ffffd95a2879fbfb5c6522063994
Jens Penneman <jens.penneman@web-4-you.be>
2025-04-23 23:00:26 +0200

    fix: removed the nonce stuff to enable SSG on some pages

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -1,16 +1,14 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { VercelToolbar } from "@vercel/toolbar/next";
-import { headers } from "next/headers";
 import React from "react";
 import { FC } from "react";
 
 const development = process.env.NODE_ENV === "development";
 const Tools: FC = async () => {
-    const nonce = (await headers()).get("x-nonce") ?? undefined;
     return (
         <>
-            <ReactQueryDevtools styleNonce={nonce} />
-            {development && <VercelToolbar nonce={nonce} />}
+            <ReactQueryDevtools />
+            {development && <VercelToolbar />}
         </>
     );
 };
