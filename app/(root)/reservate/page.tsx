9517b046bd4b28083ca875dfc1fa913f39063f13
Jens Penneman <jens.penneman@web-4-you.be>
2025-07-26 11:13:22 +0200

    feat: update maintenance message and remove reservation form from the reservate page

--- app/(root)/reservate/page.tsx
+++ app/(root)/reservate/page.tsx
@@ -2,7 +2,7 @@ import type { Metadata, NextPage } from "next";
 
 import React from "react";
 
-import AddReservationForm from "./form";
+import Alert, { AlertDescription } from "@/components/atoms/Alert";
 
 export const metadata: Metadata = {
     alternates: {
@@ -15,7 +15,13 @@ export const dynamic = "force-dynamic";
 
 const Page: NextPage = () => (
     <main className={"container mx-auto max-w-(--breakpoint-lg) p-2"}>
-        <AddReservationForm />
+        <Alert>
+            <AlertDescription>
+                De website is momenteel in onderhoud. We werken hard om deze
+                tegen 10 augustus weer beschikbaar te maken. Bedankt voor uw
+                geduld!
+            </AlertDescription>
+        </Alert>
     </main>
 );
 
