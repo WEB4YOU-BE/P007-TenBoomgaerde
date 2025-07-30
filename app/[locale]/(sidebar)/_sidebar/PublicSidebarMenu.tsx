b44475115ef996e9275e322450bf047db11dcc22
Jens Penneman <jens.penneman@web-4-you.be>
2025-07-31 00:32:46 +0200

    feat: refactor PublicSidebarMenu to improve structure and conditional rendering

--- app/[locale]/(sidebar)/_sidebar/PublicSidebarMenu.tsx
+++ app/[locale]/(sidebar)/_sidebar/PublicSidebarMenu.tsx
@@ -1,28 +1,55 @@
 "use client";
 
+import { CurrencyEurIcon } from "@phosphor-icons/react/dist/ssr";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
-import { SidebarGroup, SidebarGroupLabel } from "@/components/atoms/Sidebar";
+import {
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "@/components/atoms/Sidebar";
+import { Link, usePathname } from "@/i18n/navigation";
 import getUser from "@/service/authentication/getUser";
 
 const PublicSidebarMenu = () => {
+    const pathname = usePathname();
     const { data: user, isFetching: isFetchingUser } = useQuery({
         queryFn: getUser,
         queryKey: ["authenticatedUser"],
     });
 
-    if (isFetchingUser) return null;
-
-    if (user?.id) return null;
-
     return (
-        <SidebarGroup className="h-full">
-            <SidebarGroupLabel className="h-full flex flex-col justify-center">
-                Meld u aan bij uw account of maak een nieuw account aan om
-                toegang te krijgen tot de functies van de website.
-            </SidebarGroupLabel>
-        </SidebarGroup>
+        <>
+            <SidebarGroup>
+                <SidebarGroupContent>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                            <SidebarMenuButton
+                                asChild
+                                isActive={pathname === "/prices/"}
+                            >
+                                <Link href="/prices/">
+                                    <CurrencyEurIcon />
+                                    <span>Prijzen</span>
+                                </Link>
+                            </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarGroupContent>
+            </SidebarGroup>
+            {!isFetchingUser && !user?.id ? (
+                <SidebarGroup className="h-full">
+                    <SidebarGroupLabel className="h-full flex flex-col justify-center">
+                        Meld u aan bij uw account of maak een nieuw account aan
+                        om toegang te krijgen tot de functies van de website.
+                    </SidebarGroupLabel>
+                </SidebarGroup>
+            ) : null}
+        </>
     );
 };
 
