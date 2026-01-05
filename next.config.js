daae239450521ee0ca10c79dd7d6d9ae2db1b1d3
Jens Penneman <jenspenneman26@gmail.com>
2026-01-05 01:02:45 +0100

    chore: add babel-plugin-react-compiler to dependencies

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const config = {
     compiler: { removeConsole: env.NODE_ENV === "production" },
     trailingSlash: true,
     turbopack: {},
+    reactCompiler: true,
 };
 
 const withVercelToolbar = VercelToolbar();
